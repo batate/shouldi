469413b562eef404a2f5fab08d69d10ffd65299c
Bruce Tate <bruce@rapidred.com>
2014-10-31 14:33:35 -0500

    shouldi documentation for context matcher

--- lib/matchers/context_matcher.ex
+++ lib/matchers/context_matcher.ex
@@ -1,6 +1,20 @@
 defmodule ShouldI.Matchers.Context do
   import ExUnit.Assertions
+  @moduledoc """
+  Convenience macros for generating short test cases of common strucutre. These matchers work with the context.
+  """
   
+  @doc """
+  Exactly match a key in the context to a value. 
+  
+  ## exmaples: 
+  setup context do
+    assign context, key_from_context_returned_by_setup: "exact expected value"
+  end
+  
+  should_assign_key key_from_context_returned_by_setup: "exact expected value"
+    
+  """
   defmacro should_assign_key [{key, value}|_] do
     quote do
       should "should set #{unquote key} to #{unquote value}", context do
@@ -9,6 +23,15 @@ defmodule ShouldI.Matchers.Context do
     end
   end
 
+  @doc """
+  Pattern match against context[key]
+  
+  ## exmaples: 
+  ...
+    
+  should_match_key context_key: {:ok, _}
+    
+  """
   defmacro should_match_key [{key, expected}|_] do
     quote do
       should "should match context[#{unquote key}]  to #{Macro.to_string(unquote(Macro.escape(expected)))}", context do
@@ -18,6 +41,15 @@ defmodule ShouldI.Matchers.Context do
     end
   end
 
+  @doc """
+  Check for existance of a key in the context returned by setup.
+  
+  ## exmaples: 
+  ...
+    
+  should_have_key :must_be_present
+    
+  """
   defmacro should_have_key key do
     quote do
       should "should have key #{unquote key}", context do
@@ -26,6 +58,15 @@ defmodule ShouldI.Matchers.Context do
     end
   end
   
+  @doc """
+  Negative check for existance of a key in the context returned by setup.
+  
+  ## exmaples: 
+  ...
+    
+  should_not_have_key :must_not_be_present
+    
+  """
   defmacro should_not_have_key key do
     quote do
       should "should not have key #{unquote key}", context do
