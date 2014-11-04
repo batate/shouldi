5d0dcc7175ab87062bc48e26adb769394303d0cd
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2014-11-04 15:31:25 +0100

    Remove unnecessary "should"

--- lib/matchers/context_matcher.ex
+++ lib/matchers/context_matcher.ex
@@ -3,21 +3,21 @@ defmodule ShouldI.Matchers.Context do
   @moduledoc """
   Convenience macros for generating short test cases of common strucutre. These matchers work with the context.
   """
-  
+
   @doc """
-  Exactly match a key in the context to a value. 
-  
-  ## exmaples: 
+  Exactly match a key in the context to a value.
+
+  ## exmaples:
   setup context do
     assign context, key_from_context_returned_by_setup: "exact expected value"
   end
-  
+
   should_assign_key key_from_context_returned_by_setup: "exact expected value"
-    
+
   """
-  defmacro should_assign_key [{key, value}|_] do
+  defmacro should_assign_key [{key, value}] do
     quote do
-      should "should set #{unquote key} to #{unquote value}", context do
+      should "set #{unquote key} to #{unquote value}", context do
         assert context[unquote(key)] == unquote(value)
       end
     end
@@ -25,54 +25,53 @@ defmodule ShouldI.Matchers.Context do
 
   @doc """
   Pattern match against context[key]
-  
-  ## exmaples: 
+
+  ## exmaples:
   ...
-    
+
   should_match_key context_key: {:ok, _}
-    
+
   """
-  defmacro should_match_key [{key, expected}|_] do
+  defmacro should_match_key [{key, expected}] do
     quote do
-      should "should match context[#{unquote key}]  to #{Macro.to_string(unquote(Macro.escape(expected)))}", context do
-        
-        assert unquote(expected) = context[unquote(key)] 
+      should "match context[#{unquote key}]  to #{Macro.to_string(unquote(Macro.escape(expected)))}", context do
+
+        assert unquote(expected) = context[unquote(key)]
       end
     end
   end
 
   @doc """
   Check for existance of a key in the context returned by setup.
-  
-  ## exmaples: 
+
+  ## exmaples:
   ...
-    
+
   should_have_key :must_be_present
-    
+
   """
   defmacro should_have_key key do
     quote do
-      should "should have key #{unquote key}", context do
+      should "have key #{unquote key}", context do
         assert Enum.member?( (Dict.keys context), unquote(key))
       end
     end
   end
-  
+
   @doc """
   Negative check for existance of a key in the context returned by setup.
-  
-  ## exmaples: 
+
+  ## exmaples:
   ...
-    
+
   should_not_have_key :must_not_be_present
-    
+
   """
   defmacro should_not_have_key key do
     quote do
-      should "should not have key #{unquote key}", context do
+      should "not have key #{unquote key}", context do
         assert !Enum.member?( (Dict.keys context), unquote(key))
       end
     end
   end
-  
 end
