5b996acdd050af2c8da46c6895962f3af1cccc76
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2015-03-24 13:06:31 -0700

    Release v0.2.2

--- mix.exs
+++ mix.exs
@@ -3,14 +3,14 @@ defmodule Shouldi.Mixfile do
 
   def project do
     [app: :shouldi,
-     version: "0.2.1",
+     version: "0.2.2",
      elixir: "~> 1.0",
      deps: deps,
      name: "ShouldI",
      source_url: "https://github.com/batate/shouldi",
      docs: fn ->
        {ref, 0} = System.cmd("git", ["rev-parse", "--verify", "--quiet", "HEAD"])
-       [source_ref: ref, main: "README", readme: true]
+       [source_ref: ref, main: "README", readme: "README.md"]
      end,
      description: "Elixir testing libraries with support for nested contexts",
      package: package]
