858540dc9ebaa7ba2e7b03781b6bfef057b91ea5
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2016-01-04 12:48:12 +0100

    Release v0.3.0

--- mix.exs
+++ mix.exs
@@ -3,14 +3,14 @@ defmodule Shouldi.Mixfile do
 
   def project do
     [app: :shouldi,
-     version: "0.2.3",
+     version: "0.3.0",
      elixir: "~> 1.0",
      deps: deps,
      name: "ShouldI",
      source_url: "https://github.com/batate/shouldi",
      docs: fn ->
        {ref, 0} = System.cmd("git", ["rev-parse", "--verify", "--quiet", "HEAD"])
-       [source_ref: ref, main: "README", readme: "README.md"]
+       [source_ref: ref, main: "extra-readme", extras: ["README.md"]]
      end,
      description: "Elixir testing libraries with support for nested contexts",
      package: package]
@@ -24,12 +24,12 @@ defmodule Shouldi.Mixfile do
   end
 
   defp deps do
-    [{:ex_doc, only: :dev},
-     {:earmark, only: :dev}]
+    [{:ex_doc, ">= 0.0.0", only: :dev},
+     {:earmark, ">= 0.0.0", only: :dev}]
   end
 
   defp package do
-    [contributors: ["Bruce Tate", "Eric Meadows-Jönsson"],
+    [maintainers: ["Bruce Tate", "Eric Meadows-Jönsson"],
      licenses: ["Apache 2.0"],
      links: %{"Github" => "https://github.com/batate/shouldi"}]
   end
